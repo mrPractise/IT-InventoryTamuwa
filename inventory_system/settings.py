e7927773d61e17145f42389355e7d0ca6da91d3d
Mickey Okoth <coddingpractise@gmail.com>
2026-03-08 21:45:55 +0300

    Refactor DATABASES configuration in settings.py to use environment variable for DATABASE_URL and enable SSL connections.

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -115,24 +115,13 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 # DATABASE
 # ===============================
 
-DATABASE_URL = config('DATABASE_URL', default='')
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
-DATABASES['default']['ATOMIC_REQUESTS'] = True
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 
 
 # ===============================
